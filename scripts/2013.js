2a004fa9556ca9ae35f19152c35a7ef0ef9c0d24
Patrick Heneise <patrick.heneise@gmail.com>
2013-06-06 20:52:44 +0200

    grunt

---
+++ scripts/2013.js
@@ -0,0 +1,64 @@
+var bcnjs_events = [
+	{
+		date: [2013, 0, 3]
+	},
+	{
+		date: [2013, 1, 7]
+	},
+	{
+		date: [2013, 2, 7]
+	},
+	{
+		date: [2013, 3, 4]
+	},
+	{
+		date: [2013, 4, 2]
+	},
+	{
+		date: [2013, 5, 6],
+		talks: [
+			{
+				title: 'Building a static web page with Grunt  ...',
+				presenter: {
+					username: 'drpicox',
+					displayname: 'Dr. David Rodenas'
+				},
+				description: '**... and bower, bootstrap, coffeescript, markdown, angujarJS, less.** Grunt is the *Makefile* for JS projects. In this talk I present what is Grunt, which is its philosophy, and a small example how to use it. And the best of it, how to build and optimize everything with a simple command.'
+			},
+			{
+				title: 'Try a little tenderness',
+				presenter: {
+					username: 'masylym',
+					displayname: 'Pau Ramon'
+				},
+				description: '**Stop writing ugly Javascript, you don\'t have to.** This talk is about enforcing good practices for building and maintaining big Javascript applications. I\'m going to share what we learned while building Teambox (more than 200k lines of javascript code)'
+			},
+			{
+				title: 'GPU Deep Dive',
+				presenter: {
+					username: 'xbeumala',
+					displayname: 'Xavi Beumala'
+				},
+				description: '**with a dose of browser internals understanding** Let\'s talk about rendering performance, how to boost pages speed and make 60+ FPS animations possible. Clunky and janky scrolls? Jumpy animatios? Unresponsive UIs? Let’s see how to identify, diagnose and fix reflows, composites, repaints, textures and automatic layer creation even for low end devices without die trying.'
+			}
+		],
+	},
+	{
+		date: [2013, 6, 4]
+	},
+	{
+		date: [2013, 7, 1]
+	},
+	{
+		date: [2013, 8, 5]
+	},
+	{
+		date: [2013, 9, 3]
+	},
+	{
+		date: [2013, 10, 7]
+	},
+	{
+		date: [2013, 11, 5]
+	}
+]
