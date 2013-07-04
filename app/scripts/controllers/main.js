0b982f3de0ccfb06a57c774aa0384cd0c4aecea5
Patrick Heneise <patrick.heneise@gmail.com>
2013-07-04 11:15:17 +0200

    fixed empty talks bug

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,7 +5,7 @@ angular.module('bcnjs')
 		var i = 0;
 		var today = new Date();
 
-		while(new Date(bcnjs_events[i].date[0], bcnjs_events[i].date[1], bcnjs_events[i].date[2]) <= new Date(today.getFullYear(), today.getMonth(), today.getDate())) {
+		while(new Date(bcnjs_events[i].date[0], bcnjs_events[i].date[1], bcnjs_events[i].date[2]) <= new Date(today.getFullYear(), today.getMonth(), today.getDate() - 1)) {
 			i++;
 		}
 
