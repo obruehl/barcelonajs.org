7b01246eb11fdf7f817c42a76c7783fbfea1bdb5
Patrick Heneise <patrick.heneise@gmail.com>
2013-12-28 13:52:24 +0100

    2014 update

---
+++ app/scripts/2014.js
@@ -0,0 +1,52 @@
+// DATA FORMAT:
+// events : [
+//      { date: new Date( <year>, <month - 1>, <day>),
+//        talks:  /*optional*/ [ ... ],
+//        special: /*optional*/ { ... },
+//      }
+// ]
+var bcnjs_events = [
+	{
+		date: new Date(2014, 0, 9)
+	},
+  {
+    date: new Date(2014, 0, 25),
+    special: {
+			header: 'AngularJS Special',
+			content: ''
+		}
+  },
+	{
+		date: new Date(2014, 1, 13)
+	},
+	{
+		date: new Date(2014, 2, 13)
+	},
+	{
+		date: new Date(2014, 3, 10)
+	},
+	{
+		date: new Date(2014, 4, 8)
+	},
+	{
+		date: new Date(2014, 5, 12)
+	},
+	{
+		date: new Date(2014, 6, 10)
+	},
+	{
+		date: new Date(2014, 7, 14)
+	},
+	{
+		date: new Date(2014, 8, 11)
+	},
+	{
+		date: new Date(2014, 9, 9)
+	},
+	{
+		date: new Date(2014, 10, 13)
+	},
+	{
+		date: new Date(2014, 11, 11)
+	}
+];
