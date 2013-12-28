5d9b6dcff70e8d67dbb079e42eb511fc57ff29b6
Patrick Heneise <patrick.heneise@gmail.com>
2013-12-28 14:00:44 +0100

    2014 update

--- app/scripts/2014.js
+++ app/scripts/2014.js
@@ -7,7 +7,8 @@
 // ]
 var bcnjs_events = [
 	{
-		date: new Date(2014, 0, 9)
+		date: new Date(2014, 0, 9),
+    talks: []
 	},
   {
     date: new Date(2014, 0, 25),
@@ -17,36 +18,47 @@ var bcnjs_events = [
 		}
   },
 	{
-		date: new Date(2014, 1, 13)
+		date: new Date(2014, 1, 13),
+    talks: []
 	},
 	{
-		date: new Date(2014, 2, 13)
+		date: new Date(2014, 2, 13),
+    talks: []
 	},
 	{
-		date: new Date(2014, 3, 10)
+		date: new Date(2014, 3, 10),
+    talks: []
 	},
 	{
-		date: new Date(2014, 4, 8)
+		date: new Date(2014, 4, 8),
+    talks: []
 	},
 	{
-		date: new Date(2014, 5, 12)
+		date: new Date(2014, 5, 12),
+    talks: []
 	},
 	{
-		date: new Date(2014, 6, 10)
+		date: new Date(2014, 6, 10),
+    talks: []
 	},
 	{
-		date: new Date(2014, 7, 14)
+		date: new Date(2014, 7, 14),
+    talks: []
 	},
 	{
-		date: new Date(2014, 8, 11)
+		date: new Date(2014, 8, 11),
+    talks: []
 	},
 	{
-		date: new Date(2014, 9, 9)
+		date: new Date(2014, 9, 9),
+    talks: []
 	},
 	{
-		date: new Date(2014, 10, 13)
+		date: new Date(2014, 10, 13),
+    talks: []
 	},
 	{
-		date: new Date(2014, 11, 11)
+		date: new Date(2014, 11, 11),
+    talks: []
 	}
 ];
